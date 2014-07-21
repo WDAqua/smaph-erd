78463a70b368a402c510265301d966c84d4fbd33
marcocor <cornolti@di.unipi.it>
2014-07-21 15:11:35 +0200

    added copyright notice

--- src/main/java/it/acubelab/erd/learn/GenerateModel.java
+++ src/main/java/it/acubelab/erd/learn/GenerateModel.java
@@ -1,8 +1,22 @@
+/**
+ *  Copyright 2014 Marco Cornolti
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package it.acubelab.erd.learn;
 
-import it.acubelab.batframework.metrics.Metrics;
 import it.acubelab.batframework.metrics.MetricsResultSet;
-import it.acubelab.batframework.systemPlugins.WikiSenseAnnotator;
 import it.acubelab.batframework.utils.FreebaseApi;
 import it.acubelab.batframework.utils.WikipediaApiInterface;
 import it.acubelab.erd.SmaphAnnotator;
@@ -10,19 +24,13 @@ import it.acubelab.erd.SmaphAnnotatorDebugger;
 import it.acubelab.tagme.develexp.WikiSenseAnnotatorDevelopment;
 import it.cnr.isti.hpc.erd.WikipediaToFreebase;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Vector;
-
-import org.apache.commons.lang3.tuple.Triple;
+import java.util.*;
 
 import libsvm.svm;
 import libsvm.svm_model;
 import libsvm.svm_problem;
+
+import org.apache.commons.lang3.tuple.Triple;
 
 public class GenerateModel {
 
